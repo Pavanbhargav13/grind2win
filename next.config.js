9d3004912e60df64da087795401be9ba0451a732
Pavan Bhargav MN <pavanbhargav07@gmail.com>
2025-05-21 20:20:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  compiler: {
-    styledComponents: true,
-  },
   reactStrictMode: true,
-  swcMinify: true,
-};
+  experimental: {
+    appDir: true
+  },
+  compiler: {
+    styledComponents: true
+  }
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig
